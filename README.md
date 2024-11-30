7f555bde7c274dbb3b2c5176c7a9d73a2c323759
Alejandro <email>
2024-11-30 21:54:38 +0100

    Added linter and readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # notes-backend
+This repository contains the code to run the full notes web application related to the notes project developed on the [fullstackopen](https://fullstackopen.com/en/) free course of the Helsinki University.
+
+More specifically, with the dist folder generated from the frontend and copied to this root folder, it can be deployed only one time.
+
+The functionality of the backend behaves the same as the ones declared in the frontend repository.
+
+## Extra
+* For testing purposes, this repository has been deployed on the free tier of the Render service.
+* The link to the frontend repository is [here](https://github.com/AlejandroGorgues/notes-frontend).
--- index.js
+++ index.js
@@ -8,16 +8,15 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
-let notes = []
 
 app.get('/', (request, response) => {
-response.send('<h1>Hello World!</h1>')
+  response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notes => {
-        response.json(notes)
-      })
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
@@ -30,8 +29,8 @@ app.put('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.get('/api/notes/:id', (request, response, next) =>{
-    Note.findById(request.params.id)
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
     .then(note => {
       if (note) {
         response.json(note)
@@ -44,28 +43,28 @@ app.get('/api/notes/:id', (request, response, next) =>{
 
 app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndDelete(request.params.id)
-  .then(result => {
-    response.status(204).end()
-  })
-  .catch(error => next(error))
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    if (!body.content) {
-        return response.status(400).json({ 
-        error: 'content missing' 
-        })
-    }
-    
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
-      })
-    note.save().then(savedNote => {
-    response.json(savedNote)
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
     })
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
     .catch(error => next(error))
 })
 
@@ -83,7 +82,7 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
-  } 
+  }
 
   next(error)
 }
@@ -92,5 +91,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
