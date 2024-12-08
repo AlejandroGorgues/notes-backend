7bdba959260b9361ca30b89fe03834f383daa652
Alejandro <email>
2024-12-08 14:09:45 +0100

    feat: added endpoints for playwright testing

--- controllers/login.js
+++ controllers/login.js
@@ -5,11 +5,12 @@ const User = require('../models/user')
 
 loginRouter.post('/', async (request, response) => {
   const { username, password } = request.body
-
+  console.log('hola')
   const user = await User.findOne({ username })
   const passwordCorrect = user === null
     ? false
     : await bcrypt.compare(password, user.passwordHash)
+
 
   if (!(user && passwordCorrect)) {
     return response.status(401).json({
@@ -28,7 +29,6 @@ loginRouter.post('/', async (request, response) => {
     process.env.SECRET,
     { expiresIn: 60*60 }
   )
-
   response
     .status(200)
     .send({ token, username: user.username, name: user.name })
